df72a3a99747165fae7a8ec2bd68d5bc63d62735
HarendraRathnayaka <it21199080@my.sliit.lk>
2024-03-08 19:15:33 +0530

    commit 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,26 @@
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import {
+  BrowserRouter,
+  Router,
+  Route,
+  Routes,
+  Navigate,
+} from 'react-router-dom';
+
+import Home from './components/home';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          
+          <Route path="/home" element={<Home/>}/>
+
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ frontend/src/components/home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import header from './header'
+import footer from './footer'
+
+function Home() {
+  return (
+    <>
+        <header/>
+
+            <div>
+
+            </div>
+
+        <footer/>
+    </>
+  )
+}
+
+export default Home;
