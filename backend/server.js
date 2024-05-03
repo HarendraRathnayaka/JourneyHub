1afbcb9cad16b718b852cc308258d75ff8851634
Sihil Rashmika <it21206696@my.sliit.lk>
2024-05-03 18:09:02 +0530

    Server Js commit
    MongoDB to connect server js

--- backend/server.js
+++ backend/server.js
@@ -1,59 +1,21 @@
-const express = require('express');
+const express = require('express')
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 5000
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const app = express();
-require('dotenv').config();
+const guideRouter = require('./routes/guide')
 
+mongoose.connect(process.env.DB_URI);
 
-app.use(
-  cors({
-    origin: 'http://localhost:3000',
-    methods: 'GET,POST,PUT,DELETE',
-    credentials: true,
-  })
-);
+const db = mongoose.connection
+db.once('open', () => console.log('Connected to DB!'))
 
+app.use(express.json())
 
-// Database Connection
-const URL = process.env.MONGODB_URL;
-
-mongoose.connect(URL, {
-  //useCreateIndex: true,
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  //useFindAndModify: false
-});
-
-const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log('Mongodb database connected!');
-});
-
-// Middleware
-app.use(cors());
-app.use(express.json());
-
-
-// https://localhost/guide/
-app.use('/guide', require('./routes/tourguide.js'));
+app.use('/api/guide',guideRouter)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-const PORT = process.env.PORT || 8070;
-app.listen(PORT, () => {
-  console.log(`Server is up and running on port ${PORT}`);
-});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
