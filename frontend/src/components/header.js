412aa8df20cfeabdce61d4b444164d44b7742a2b
Sihil Rashmika <117860667+rashmikasihil@users.noreply.github.com>
2024-05-07 22:54:47 +0530

    Update header.js

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+// Define a functional component named Header
 function header() {
   return (
     <div>
@@ -8,4 +9,5 @@ function header() {
   )
 }
 
+// Export the Header component as the default export
 export default header
